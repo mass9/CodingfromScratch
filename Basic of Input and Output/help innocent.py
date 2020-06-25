01259fa5867cb130c83a0c1d37c5cf6ee71a292e
Prashant <pkt785@gmail.com>
2020-06-25 22:59:46 +0530

    helping innocent

--- Basic of Input and Output/help innocent.py
+++ Basic of Input and Output/help innocent.py
@@ -1,6 +1,15 @@
-s  = input()
-if s[2] != 'A' or s[2] != 'E' or s[2] != 'I' or s[2] != 'O' or s[2] != 'U' or s[2] != 'Y':
-    
-    print('Valid')
-else:
-    print('Invalid')
+s = input()
+
+vowels = "AEIOUY"
+
+flag = False
+
+for _ in range(len(s)):
+
+        if (int(s[0])+int(s[1]))%2==0 and (int(s[3])+int(s[4]))%2==0 and (int(s[4])+int(s[5]))%2==0 and (int(s[-1])+int(s[-2]))%2==0:
+            flag = True
+
+ans = 'valid' if (flag and s[2] not in vowels) else 'invalid'
+
+print(ans)
+
