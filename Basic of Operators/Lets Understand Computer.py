bf208741538805e488ff9030559c95ce830182e6
Prashant <pkt785@gmail.com>
2020-07-01 14:28:27 +0530

    improvement on division format

--- Basic of Operators/Lets Understand Computer.py
+++ Basic of Operators/Lets Understand Computer.py
@@ -1,5 +1,19 @@
+import math
 t = int(input()) # testcases
 
+#Proper approach
+def decimalToBinary(n):
+    i =1
+    while(i <= math.sqrt(n)):
+        i = i*2
+        if (n//i >= i/2):
+            ans = n-(n//i)
+        else:
+            ans = (n-(i//2)) +1
+    print(ans)
+
+# Naive approach
+"""
 def decimalToBinary(n):
     count =0
     for i in range(1,n+1):
@@ -8,7 +22,7 @@ def decimalToBinary(n):
         if res <= i :
             count+=1
     print(count)
-
+"""
 while t>0:
     x = int(input())
     decimalToBinary(x)
