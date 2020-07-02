b129092a4e59fbd6376c2084279761be3709c1d2
Prashant <pkt785@gmail.com>
2020-07-03 01:26:05 +0530

    conject it

--- Basic of Input and Output/conject-it.py
+++ Basic of Input and Output/conject-it.py
@@ -1,14 +1,15 @@
 def conject(n):
-    if n % 2 == 0:
-        n = n/2
-        conject(n)
-    else:
-        n = 3*n +1 
-        conject(n)    
-    if n == 1 :
-        print('Yes')
-    else:
-        print('No')
+    while True:
+        if n % 2 == 0:
+            n = n//2
+            if (n==1):
+                print('YES')
+                break
+        else:
+            n = 3*n +1 
+            if (n==1):
+                print('YES')
+                break     
 
 t = int(input())
 while t >0:
