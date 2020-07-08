b887cbe9ef9fbcd6c85f0e96cacd4fece07b4351
Prashant <pkt785@gmail.com>
2020-07-09 00:29:57 +0530

    digit solved

--- Implementation/Digit Problem.py
+++ Implementation/Digit Problem.py
@@ -1,5 +1,12 @@
 s,i = input().split()
+s= list(s)
 i = int(i)
-for x in range(i):
-    s =s.replace(s[x],'9')
-print(s)
+n = ''
+for x in range(len(s)):
+    if s[x] != '9' and i > 0:
+        n += '9'
+        i -=1
+    else:
+        n += s[x]
+
+print(n)
