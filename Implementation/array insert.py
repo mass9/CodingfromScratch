d64325f53634155229e68fc2b7258b3cc41a09ce
Prashant <pkt785@gmail.com>
2020-07-14 00:59:36 +0530

    array modification

--- Implementation/array insert.py
+++ Implementation/array insert.py
@@ -2,7 +2,14 @@ n , qs = [x for x in input().split()]
 n_elements = [ int(i) for i in input().split()]
 
 for _ in range(int(qs)):
-    q = [j for j in input().split()]
-    if q[0] == '1':
-        qs[q[2]] = '5'
-    print(qs)
+    q = [int(j) for j in input().split()]
+    if q[0] == 1:
+        n_elements[q[1]] = q[2]
+        # print('n_elements:',n_elements)
+
+    else:
+        l , r = q[1] , q[2]
+        # print('l:',l)
+        # print('r:',r)
+        # print('n_elements:',n_elements)
+        print(sum(n_elements[l:r+1]))
